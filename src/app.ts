2baf93aa4036d10eba05ec24ce4ea9f28f38196b
Salah Ashraf <salahashraf924@gmail.com>
2023-04-13 23:24:56 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,50 +1,63 @@
-import express from 'express'
-import buildDatabase from './DataBaseConnection'
-require('dotenv').config();
-import validationTagModel from './model/ValidationTag';
-import mongoose from 'mongoose';
-import testCaseModel from './model/TestCase';
+import express from "express";
+import buildDatabase from "./DataBaseConnection";
+require("dotenv").config();
+import validationTagModel from "./model/ValidationTag";
+import mongoose from "mongoose";
+import testCaseModel from "./model/TestCase";
 const { ObjectID, default: BSON } = require("bson");
 export function createApp() {
+  buildDatabase();
 
-    const app = express();
-    buildDatabase();
+  const app = express();
 
-    app.use(express.json());
+  app.use(express.json());
 
-    const validationTagExample:any={
-        metaData: {
-            "name": "validationTag1",
-            "description": "This is a validationTag",
-            "testSuiteRef": "testSuite1",
+  const validationTagExample: any = {
+    metaData: {
+      name: "validationTag1",
+      description: "This is a validationTag",
+      testSuiteRef: "testSuite1",
+    },
+    isSuccessful: true,
+  };
+
+  const testCase: any = {
+    metaData: {
+      name: "testCase1",
+      description: "This is a testCase",
+    },
+    isSuccessful: false,
+    validationTagRefs: [
+      new mongoose.Types.ObjectId("64349e56cea28645feb83ffb"),
+    ],
+  };
+  app.get("/validationTags", async (req, res) => {
+    const ret: any = await validationTagModel.create(validationTagExample);
+    return res.status(200).send(ret);
+  });
+  app.post("/testCase", async (req, res) => {
+    const ret: any = await testCaseModel.create(testCase);
+    return res.status(200).send(ret);
+  });
+  app.put("/testCase", async (req, res) => {
+    // let testCase =new testCaseModel(testCaseModel.findById("6434a1a5e7905fe655a5b0b1"));
+    // testCase.isSuccessful=true;
+    // const ret:any=await testCase.save();
+    // return res.status(200).send(ret);
+    const _id="6434a1a5e7905fe655a5b0b1";
+
+    const ret = await testCaseModel.updateOne(
+      { _id },
+      {
+        $set: {
+          metaData: {
+            name2: "hazem",
+          },
         },
-        isSuccessful: true,
-    };
+      }
+    );
+    return res.status(200).send(ret);
+  });
 
-    const testCase :any={
-        metaData: {
-            "name": "testCase1",
-            "description": "This is a testCase",
-        },
-        isSuccessful:false,
-        validationTagRefs:[new mongoose.Types.ObjectId("64349e56cea28645feb83ffb")]
-    }
-     app.get('/validationTags', async(req, res) => {
-        const ret:any=await validationTagModel.create(validationTagExample);;
-        return res.status(200).send(ret);
-        
-    })
-    app.post('/testCase', async(req, res) => {
-        const ret:any=await testCaseModel.create(testCase);
-        return res.status(200).send(ret);
-        
-    })
-    app.put('/testCase', async(req, res) => {
-        let testCase =new testCaseModel(testCaseModel.findById("6434a1a5e7905fe655a5b0b1")); 
-        testCase.isSuccessful=true;
-        const ret:any=await testCase.save();
-        return res.status(200).send(ret);
-    })
-
-    return app
+  return app;
 }
