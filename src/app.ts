53208223655e31bad55401b0e21cab86e7394673
Salah Ashraf <salahashraf924@gmail.com>
2023-04-14 15:39:45 +0200

    add TestSuite routes to the app

--- src/app.ts
+++ src/app.ts
@@ -1,41 +1,34 @@
-import express from 'express'
-import buildDatabase from './DataBaseConnection'
-require('dotenv').config();
-import validationTagModel from './model/ValidationTag';
-import mongoose from 'mongoose';
-import testCaseModel from './model/TestCase';
+import express from "express";
+import buildDatabase from "./DataBaseConnection";
+require("dotenv").config();
 
-import swaggerUi from 'swagger-ui-express'
-import swaggerJsdoc from 'swagger-jsdoc'
+import swaggerUi from "swagger-ui-express";
+import swaggerJsdoc from "swagger-jsdoc";
+import TestSuiteRoutes from "./routes/TestSuiteRoutes";
 
-const { ObjectID, default: BSON } = require("bson");
 export function createApp() {
+  const app = express();
+
   buildDatabase();
 
-    const app = express();
-
-
-    buildDatabase();
-
   app.use(express.json());
+  app.use(TestSuiteRoutes);
 
-    const options = {
-        definition: {
-            openapi: '3.0.0',
-            info: {
-            title: 'Hello World',
-            version: '1.0.0',
-            },
-        },
-        apis: ['./src/components/**/*.openapi.yaml'],
-    };
+  const options = {
+    definition: {
+      openapi: "3.0.0",
+      info: {
+        title: "Hello World",
+        version: "1.0.0",
+      },
+    },
+    apis: ["./src/components/**/*.openapi.yaml"],
+  };
 
-    const openapiSpecification = swaggerJsdoc(options);
+  const openapiSpecification = swaggerJsdoc(options);
 
-    app.use('/api', swaggerUi.serve, swaggerUi.setup(openapiSpecification))
+  app.use("/api", swaggerUi.serve, swaggerUi.setup(openapiSpecification));
 
-
-
-
-    return app
+  
+  return app;
 }
